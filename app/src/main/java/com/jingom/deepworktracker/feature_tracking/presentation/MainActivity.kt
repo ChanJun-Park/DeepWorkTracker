3d505d7fd87d5d6a338b97a6c44c148418364cd9
박찬준 <pcj01142@gmail.com>
2022-03-21 22:31:01 +0900

    MainActivity 설정

---
+++ app/src/main/java/com/jingom/deepworktracker/feature_tracking/presentation/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.jingom.deepworktracker.feature_tracking.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.jingom.deepworktracker.R
+import com.jingom.deepworktracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityMainBinding
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		binding = ActivityMainBinding.inflate(layoutInflater)
+
+		setContentView(binding.root)
+
+		initSystemUIColor()
+
+		initWindowEdgeToEdgeConfig()
+	}
+
+	private fun initSystemUIColor() {
+		WindowInsetsControllerCompat(window, binding.root).apply {
+			isAppearanceLightStatusBars = true
+			isAppearanceLightNavigationBars = true
+		}
+	}
+
+	private fun initWindowEdgeToEdgeConfig() {
+		WindowCompat.setDecorFitsSystemWindows(window, false)
+	}
+}
