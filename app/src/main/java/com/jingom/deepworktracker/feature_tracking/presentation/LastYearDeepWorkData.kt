dba250692057bd159a73f1530933e26af454108b
박찬준 <pcj01142@gmail.com>
2022-04-09 23:42:39 +0900

    불필요한 코드 제거

--- app/src/main/java/com/jingom/deepworktracker/feature_tracking/presentation/LastYearDeepWorkData.kt
+++ app/src/main/java/com/jingom/deepworktracker/feature_tracking/presentation/LastYearDeepWorkData.kt
@@ -5,11 +5,6 @@ import com.jingom.deepworktracker.feature_tracking.domain.model.DeepWork
 import com.jingom.deepworktracker.feature_tracking.domain.model.DeepWorkTimesOnDay
 import java.time.LocalDate
 
-data class DashBoardState(
-	val deepWorks: List<DeepWork>,
-	val deepWorkTimesOnDayInLastYear: LastYearDeepWorkData
-)
-
 data class LastYearDeepWorkData(
 	val baseDate: LocalDate = LocalDateTimes.now().toLocalDate(),
 	val deepWorkRecordMap: Map<LocalDate, DeepWorkTimesOnDay> = emptyMap()
