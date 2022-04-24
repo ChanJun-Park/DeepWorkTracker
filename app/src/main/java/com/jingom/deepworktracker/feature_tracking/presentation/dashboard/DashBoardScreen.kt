4b264155d85ad0fb9ff4f0938a0ae11d20a56e03
박찬준 <pcj01142@gmail.com>
2022-04-24 21:39:51 +0900

    대쉬보드 딥 워크 시작 FAB 수정

--- app/src/main/java/com/jingom/deepworktracker/feature_tracking/presentation/dashboard/DashBoardScreen.kt
+++ app/src/main/java/com/jingom/deepworktracker/feature_tracking/presentation/dashboard/DashBoardScreen.kt
@@ -1,15 +1,17 @@
 package com.jingom.deepworktracker.feature_tracking.presentation.dashboard
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.jingom.deepworktracker.R
 import com.jingom.deepworktracker.feature_tracking.presentation.dashboard.components.YearRecord
 import com.jingom.deepworktracker.feature_tracking.presentation.util.Screen
 
@@ -24,17 +26,13 @@ fun DashBoardScreen(
 	Scaffold(
 		scaffoldState = scaffoldState,
 		floatingActionButton = {
-			Button(
-				modifier = Modifier
-					.width(100.dp)
-					.height(100.dp),
-				onClick = {
-					navController.navigate(route = Screen.DeepWorkScreen.route)
-				},
-			) {
-				Text("Start Deep Work")
-			}
-		}
+			ExtendedFloatingActionButton(
+				icon = { Icon(Icons.Filled.PlayArrow, contentDescription = stringResource(id = R.string.record_deep_work)) },
+				text = { Text(stringResource(id = R.string.record_deep_work)) },
+				onClick = { navController.navigate(route = Screen.DeepWorkScreen.route) }
+			)
+		},
+		floatingActionButtonPosition = FabPosition.Center
 	) {
 		Box(
 			modifier = Modifier
